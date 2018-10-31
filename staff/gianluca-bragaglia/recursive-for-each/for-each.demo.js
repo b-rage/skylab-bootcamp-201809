6f70384c3deb7abd2e61ab418ffe798627a0b9a7
Gianluca <gianluca.bragaglia@gmail.com>
2018-10-31 10:57:10 +0100

    start

---
+++ staff/gianluca-bragaglia/recursive-for-each/for-each.demo.js
@@ -0,0 +1,8 @@
+const forEach = require('./for-each')
+
+const nums = [1, 2, 3]
+
+forEach(nums, (num, index) => console.log(num))
+// 1
+// 2
+// 3
---
+++ staff/gianluca-bragaglia/recursive-for-each/for-each.js
@@ -0,0 +1,15 @@
+
+
+function forEach(nums, index) {
+    
+    if(index < nums.length-1)
+    console.log(nums[index]);   
+    x++
+    forEach()
+} 
+
+function forEach(array, callback) {
+    // TODO
+  }
+  
+  module.exports = forEach
