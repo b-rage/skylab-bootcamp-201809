0a23dfbcebc251916bf06354f921e939d36f20df
Gianluca <gianluca.bragaglia@gmail.com>
2018-11-14 17:07:13 +0100

    updates

--- staff/gianluca-bragaglia/kanban-app/src/components/Profile.js
+++ staff/gianluca-bragaglia/kanban-app/src/components/Profile.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { Route, withRouter, Redirect } from 'react-router-dom'
-import EditProfile from './EditProfile'
 import logic from '../logic'
 
 class Profile extends Component {
 
-    state = { name: '', surname: '', username: '' }
+    state = { name: '', surname: '', username: '', img: null }
 
     componentDidMount() {
         try {
@@ -17,6 +16,21 @@ class Profile extends Component {
         }
     }
 
+    uploadWidget =() => {
+        let widget = window.cloudinary.openUploadWidget({ cloud_name: 'dmp64syaz', upload_preset: 'ti3e8knr'},
+            (error, result) => {
+               
+                if (result.event === "success") {
+                    const img = result.info.secure_url
+
+                    this.setState({ img })
+                    
+                    widget.close()
+                }
+            })
+            console.log()
+    }
+
     handleEditClick = () => this.props.history.push('/edit') 
 
 
@@ -24,7 +38,8 @@ class Profile extends Component {
 
         return<div className='profile-container'>
             <div className='profile-container-left'>
-                
+            <img className="post__img" src={this.state.img} ></img>
+            <button onClick={this.uploadWidget} className="btn-logout">Add Image</button>
             </div>
             <div className='profile-container-right'>
                 <p className='profile-info'>name: {this.state.name}</p>
